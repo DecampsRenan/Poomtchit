474f9dd13515fd783137df251b4b131261abbb79
Renan <renan.decamps@gmail.com>
2022-01-20 19:27:51 +0100

    feat: add global control over all samples (play / pause) and single controls (go to begin, play / pause, mute)

--- src/components/SoundCard/index.tsx
+++ src/components/SoundCard/index.tsx
@@ -1,13 +1,30 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 
 import { Button, ButtonGroup, Flex } from '@chakra-ui/react';
 import * as Tone from 'tone';
 
 export const SoundCard = ({ audioBuffer }) => {
   const [isPlaying, setIsPlaying] = useState(false);
+  const [isMuted, setIsMuted] = useState(false);
   const playerRef = useRef<Tone.Player>(null);
 
   const togglePlay = () => setIsPlaying(!isPlaying);
+
+  useEffect(() => {
+    const setPlaying = () => {
+      setIsPlaying(true);
+    };
+    const setPause = () => {
+      setIsPlaying(false);
+    };
+    Tone.Transport.on('pause', setPause);
+    Tone.Transport.on('start', setPlaying);
+
+    return () => {
+      Tone.Transport.off('pause', setPause);
+      Tone.Transport.off('start', setPlaying);
+    };
+  }, []);
 
   useEffect(() => {
     playerRef.current = new Tone.Player({
@@ -17,7 +34,7 @@ export const SoundCard = ({ audioBuffer }) => {
     return () => {
       playerRef?.current.dispose();
     };
-  });
+  }, [audioBuffer]);
 
   useEffect(() => {
     if (!playerRef?.current) return;
@@ -26,7 +43,21 @@ export const SoundCard = ({ audioBuffer }) => {
     } else {
       playerRef.current.stop();
     }
-  });
+  }, [isPlaying]);
+
+  const restart = () => {
+    playerRef?.current.restart();
+  };
+
+  const stop = () => {
+    setIsPlaying(false);
+    playerRef?.current.stop();
+  };
+
+  const toggleMute = () => {
+    playerRef?.current.set({ mute: !isMuted });
+    setIsMuted(!isMuted);
+  };
 
   return (
     <Flex flexDir="column">
@@ -37,17 +68,23 @@ export const SoundCard = ({ audioBuffer }) => {
         onClick={togglePlay}
         bgColor="cyan.200"
       >
-        {isPlaying ? 'Pause' : 'Play'}
+        {isPlaying ? 'Stop' : 'Play'}
       </Button>
       <ButtonGroup mt="-px" isAttached variant="outline" rounded="none">
-        <Button disabled mr="-px" rounded="none" w="full">
-          B
+        <Button
+          disabled={!isPlaying}
+          mr="-px"
+          rounded="none"
+          w="full"
+          onClick={restart}
+        >
+          Begin
         </Button>
-        <Button disabled mr="-px" rounded="none" w="full">
-          R
+        <Button mr="-px" rounded="none" w="full" onClick={toggleMute}>
+          {isMuted ? 'Unmute' : 'Mute'}
         </Button>
-        <Button disabled rounded="none" w="full">
-          S
+        <Button disabled={!isPlaying} rounded="none" w="full" onClick={stop}>
+          Stop
         </Button>
       </ButtonGroup>
     </Flex>
